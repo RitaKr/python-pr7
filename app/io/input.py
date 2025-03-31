18a09cbacc991dfc07643e16bea8830655dcbc93
Marharyta Kryzhanivska <ritokra@gmail.com>
2025-03-31 23:38:05 +0300

    added empty functions in io module

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def console_input():
+    pass
+
+def read_file(file_path):
+    pass
+
+def read_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def console_output(text):
+    pass
+
+def write_file(file_path, text):
+    pass
